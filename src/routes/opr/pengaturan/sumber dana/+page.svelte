8f3ce41f8726564c228dbc5db6cfc212da2306ee
Edward Manhattan <edwardmanhattan27@gmail.com>
2023-10-15 02:00:21 +0700

    sumber dana

--- src/routes/opr/pengaturan/sumber dana/+page.svelte
+++ src/routes/opr/pengaturan/sumber dana/+page.svelte
@@ -2,53 +2,42 @@
 	// @ts-nocheck
 
 	import { fiero } from '$lib/js/fiero';
-	import { getKeyModifier, shownKeyModifier } from '$lib/js/modifier';
 	import { formatTitle } from '$lib/js/string';
+	import { snack } from '$lib/js/vanilla';
 	import Modal from '$lib/modal/modal.svelte';
+	import Row from '$lib/table/row.svelte';
 	import Skeleton from '$lib/table/skeleton.svelte';
 	import Table from '$lib/table/table.svelte';
 	import Icon from '@iconify/svelte';
 	let modal;
 
-	let source = fiero(`/operator/getAllUserOperator`);
+	let source = fiero(`/operator/getListSumberDana`);
 
 	let modifier = {
-		id: { show: false },
-		foto: { show: false },
-		status: { show: false },
-		privilege: { show: false }
-	};
-
-	let modalModifier = {
-		pageNum: { show: false }
+		id: { show: false }
 	};
 
 	let buttons = [
-		{
-			head: 'Aksi',
-			icon: 'mdi:pencil',
-			action: (_, obj) => {
-				selected = obj;
-				modal.open();
-			}
-		}
+		// {
+		// 	head: 'Aksi',
+		// 	icon: 'mdi:pencil',
+		// 	action: (_, obj) => {
+		// 		selected = obj;
+		// 		modal.open();
+		// 	}
+		// }
 	];
 
 	let selected = {};
 </script>
 
 <div class="flex items-center justify-between">
-	<h1>User Operator</h1>
+	<h1>Sumber Dana</h1>
 	<div>
 		<button
 			on:click={async () => {
 				selected = {
-					nama: '',
-					username: '',
-					password: '',
-					nama_perusahaan: '',
-					no_telepon: '',
-					email: ''
+					nama: ''
 				};
 				modal.open();
 			}}
@@ -69,22 +58,25 @@
 
 <!-- svelte-ignore a11y-label-has-associated-control -->
 <Modal bind:this={modal}>
-	<h1>User Operator</h1>
+	<h1>Sumber Dana</h1>
 
-	{#each shownKeyModifier(getKeyModifier([selected], { ...modifier, ...modalModifier })) as key}
-		<label>{formatTitle(key)}</label>
-		{#if modalModifier[key]?.disabled}
-			<input type="text" bind:value={selected[key]} class="disabled" disabled />
-		{:else if modifier[key]?.type === 'datetime'}
-			<input type="date" bind:value={selected[key]} />
-		{:else if key === 'password'}
-			<input type="password" bind:value={selected[key]} />
-		{:else}
-			<input type="text" bind:value={selected[key]} />
-		{/if}
-	{/each}
+	<div>Nama Sumber Dana</div>
+	<input type="text" bind:value={selected.nama} />
 
 	<br />
 	<br />
-	<button>Simpan</button>
+	<button
+		on:click={async () => {
+			const res = await fiero(`/operator/insertSumberDana`, 'POST', selected);
+			if (res.status === 200) {
+				snack.info('Berhasil menambah sumber dana baru');
+				source = fiero(`/operator/getListSumberDana`);
+			} else {
+				snack.info('Terjadi kesalahan');
+			}
+			modal.close();
+		}}
+	>
+		Simpan
+	</button>
 </Modal>
