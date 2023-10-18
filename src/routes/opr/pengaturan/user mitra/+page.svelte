5e2adc915bf11e48fc617b262b7dbf4191afc167
Edward Manhattan <edwardmanhattan27@gmail.com>
2023-10-18 16:26:00 +0700

    user mitra

--- src/routes/opr/pengaturan/user mitra/+page.svelte
+++ src/routes/opr/pengaturan/user mitra/+page.svelte
@@ -4,6 +4,7 @@
 	import { fiero } from '$lib/js/fiero';
 	import { getKeyModifier, shownKeyModifier } from '$lib/js/modifier';
 	import { formatTitle } from '$lib/js/string';
+	import { snack } from '$lib/js/vanilla';
 	import Modal from '$lib/modal/modal.svelte';
 	import Skeleton from '$lib/table/skeleton.svelte';
 	import Table from '$lib/table/table.svelte';
@@ -44,6 +45,7 @@
 		<button
 			on:click={async () => {
 				selected = {
+					id: 0,
 					nama: '',
 					username: '',
 					password: '',
@@ -72,20 +74,44 @@
 <Modal bind:this={modal}>
 	<h1>User Mitra</h1>
 
-	{#each shownKeyModifier(getKeyModifier([selected], { ...modifier, ...modalModifier })) as key}
-		<label>{formatTitle(key)}</label>
-		{#if modalModifier[key]?.disabled}
-			<input type="text" bind:value={selected[key]} class="disabled" disabled />
-		{:else if modifier[key]?.type === 'datetime'}
-			<input type="date" bind:value={selected[key]} />
-		{:else if key === 'password'}
-			<input type="password" bind:value={selected[key]} />
-		{:else}
-			<input type="text" bind:value={selected[key]} />
-		{/if}
-	{/each}
+	<label>Nama</label>
+	<input type="text" bind:value={selected.nama} />
+	<br />
+
+	<label>Username</label>
+	<input type="text" bind:value={selected.username} />
+	<br />
+
+	<label>Password</label>
+	<input type="password" bind:value={selected.password} />
+	<br />
+
+	<label>Email</label>
+	<input type="email" bind:value={selected.email} />
+	<br />
+
+	<label>Nama Perusahaan</label>
+	<input type="text" bind:value={selected.nama_perusahaan} />
+	<br />
+
+	<label>No. Telepon</label>
+	<input type="text" bind:value={selected.no_telepon} />
 
 	<br />
 	<br />
-	<button> Simpan </button>
+	<button
+		on:click={async () => {
+			let url = 'insert';
+			if (selected.id !== 0) url = 'update';
+			const res = await fiero(`/operator/${url}UserMitra`, 'POST', selected);
+			if (res.status === 200) {
+				if (selected.id === 0) snack.info('Berhasil menambah User Mitra baru');
+				else snack.info('Berhasil mengubah informasi User Mitra');
+			} else snack.info('Terjadi Kesalahan');
+			source = fiero(`/operator/getAllUserMitra`);
+			modal.close();
+		}}
+	>
+		Simpan
+	</button>
 </Modal>
